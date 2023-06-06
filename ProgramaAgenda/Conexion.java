a8c79e9b3edcc7c36c26ec4c4ce882a2f96335c8
Sergi <sergimarenas@gmail.com>
2023-06-07 00:02:31 +0200

    Update Conexion.java

--- ProgramaAgenda/Conexion.java
+++ ProgramaAgenda/Conexion.java
@@ -1,32 +1,18 @@
+package Modelo;
 
-
-
-
-/*
-Proyecto Agenda - Archivo Conexion.java Compañía DAM
-Licencia Creative Commons BY-NC-SA 4.0
-https://creativecommons.org/licenses/by-nc-sa/4.0/
- */
-/**
- *
- * @author Sergi <sergi.marrahy at sergi.org>
- * @version 1.0
- * @date 24 may. 2023 10:19:56
- */
 import Controlador.Contacto;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class Conexion { //Modelo
+public class Conexion {
 
     public String driver = "org.mariadb.jdbc.Driver";
     public String database = "AGENDA";
     public String hostname = "localhost";
     public String port = "3306";
-
     public String url = "jdbc:mariadb://" + hostname + ":" + port + "/" + database + "?useSSL=false";
-    public String username = "usr_agenda";
-    public String password = "pwd1234";
+    public String username = "root";
+    public String password = "123456789";
     public static Connection conn = null;
 
     public void conectarMySQL() {
@@ -52,8 +38,67 @@ public class Conexion { //Modelo
         }
     }
 
+    public static boolean comprobarSiExisteEnBD(Contacto c) {
+        // busca si existe el contacto (si tiene el mismo nombre y el mismo email, por ejemplo)
+        // si existe devuelve true
+        // si no existe devuelve false
+        return false;
+    }
+
+    public static void insertarContactoEnBD(Contacto c) {
+        // insertar en BD
+        try {
+            String query = " INSERT INTO contacts (fullname, telephone, email)" + " values (?, ?, ?)";
+            
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setString(1, c.getNombre());
+            preparedStmt.setString(2, c.getTelefono());     //ABRE LOS PDFS DE ENTORNOS EN AULES Y MIRA LOS LINKS DE INSERTAR - ELIMINAR - UPDATEAR Y HAZ LOS METODOS RESTANTES
+            preparedStmt.setString(3, c.getCorreo());
+            preparedStmt.execute();
+            
+        } catch (SQLException e){
+            System.out.println("[!] No se ha podido introducir el contacto. Detalles: ");
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean eliminarContactoPorIDEnBD(Contacto c) {
+// comprueba si existe ese id
+// si existe, lo borra y devuelve true
+// si no existe, devuelve false
+        return false;
+    }
+
+    public static ArrayList<Contacto> buscarPorNombreEnBD(String cadena) {
+        // busca los contactos que tienen ese cadena o parte de ese cadena como nombre (nombre LIKE '%' + cadena + '%')
+
+        ArrayList<Contacto> contacto = new ArrayList<>();
+        return contacto;
+        // devuelve en un arraylist las coincidencias
+    }
+
+    public static ArrayList<Contacto> buscarPorEmailEnBD(String cadena) {
+// busca los contactos que tienen esa cadena en su email (email LIKE '%'+cadena+'%')
+// devuelve en un arraylist las coincidencias
+        ArrayList<Contacto> contacto = new ArrayList<>();
+        return contacto;
+    }
+
+    public static boolean buscarPorTelefonoEnBD(String cadena) {
+// busca los contactos que tienen esa cadena en su telefono (telefono LIKE '%'+cadena+'%')
+// devuelve en un arraylist las coincidencias
+        return false;
+    }
+
+    public static boolean buscarGlobalEnBD(String cadenadetexto) {
+// busca los contactos que tienen esa cadena en alguno de sus campos
+// Para ello, llama a los método anteriores y concatena los resultados distintos
+// devuelve un único arraylist con todas las coincidencias (de contactos con diferente id)
+        return false;
+    }
+
     public static ArrayList<Contacto> leerContactosDesdeBD() {
-        ArrayList<Contacto> al_contactos = new ArrayList<Contacto>();
+        ArrayList<Contacto> contactos = new ArrayList<Contacto>();
         Contacto contactoActual;
         try {
             Statement stmt = conn.createStatement();
@@ -61,158 +106,12 @@ public class Conexion { //Modelo
             ResultSet res = stmt.executeQuery(consulta);
             while (res.next()) {
                 contactoActual = new Contacto(res.getString(1), res.getString(2), res.getString(3));
-                al_contactos.add(contactoActual);
+                contactos.add(contactoActual);
             }
         } catch (SQLException e) {
             System.out.println("[!] No se ha podido leer el contacto. Detalles: ");
             e.printStackTrace();
         }
-        return al_contactos;
-    }
-
-    public static ArrayList<Contacto> buscarPorNombreEnBD(String cadena) {
-        ArrayList<Contacto> al_contactos = new ArrayList<Contacto>();
-        Contacto contactoActual;
-        try {
-            Statement stmt = conn.createStatement();
-            String consulta = "SELECT fullname, telephone, email FROM CONTACTS WHERE fullname LIKE '%" + cadena + "%'";
-            ResultSet res = stmt.executeQuery(consulta);
-            while (res.next()) {
-                contactoActual = new Contacto(res.getString(1), res.getString(2), res.getString(3));
-                al_contactos.add(contactoActual);
-            }
-        } catch (SQLException e) {
-            System.out.println("[!] No se ha podido leer el contacto. Detalles: ");
-            e.printStackTrace();
-        }
-        return al_contactos;
-    }
-
-    public static void insertarContactoEnBD(Contacto c) {
-        // insertar en BD
-        Contacto nuevoContacto = c;
-        String nombre = nuevoContacto.getNombre();
-        String telephone = nuevoContacto.getTelefono();
-        String email = nuevoContacto.getCorreo();
-        try {
-            Statement stmt = conn.createStatement();
-            String consulta = "INSERT INTO contacts(nombre, telephone, email)" + "values (?, ?, ?)";            
-            PreparedStatement prepareStmt = conn.prepareStatement(consulta);
-            prepareStmt.setString(1, nombre);
-            prepareStmt.setString(2, telephone);
-            prepareStmt.setString(3, email);
-            
-            prepareStmt.execute();
-        } catch (SQLException e) {
-            System.out.println("[!] No se ha podido crear el contacto. Detalles: ");
-            e.printStackTrace();
-        }
-    }
-
-    public static boolean eliminarContactoPorIDEnBD(Contacto c) {
-        // comprueba si existe ese id
-        Contacto contactoParaEliminar = c;
-        String nombre = contactoParaEliminar.getNombre();
-        String telephone = contactoParaEliminar.getTelefono();
-        String email = contactoParaEliminar.getCorreo();
-        
-        try {
-            String consulta = "SELECT fullname, telephone, email FROM contacts WHERE fullname = ? AND telephone = ? AND email = ?";
-            PreparedStatement prepareStmt = conn.prepareStatement(consulta);
-            prepareStmt.setString(1, nombre);
-            prepareStmt.setString(2, telephone);
-            prepareStmt.setString(3, email);
-            ResultSet res = prepareStmt.executeQuery(consulta);
-            
-            if (res.next()) {
-                int contactID = res.getInt("contactid");
-            }
-            
-            
-        } catch (SQLException e) {
-            System.out.println("[!] No se ha podido crear el contacto. Detalles: ");
-            e.printStackTrace();
-        }
-    }
-
-    public static ArrayList<Contacto> buscarGlobal(String cadena) {
-        
-        ArrayList<Contacto> al_contactos = new ArrayList<Contacto>();
-        Contacto contactoActual;
-        try {
-            Statement stmt = conn.createStatement();
-            String consulta = "SELECT fullname, telephone, email FROM CONTACTS WHERE fullname LIKE '%" + cadena + "%' OR telephone LIKE '%" + cadena + "%' OR email LIKE '%" + cadena + "%'";
-            ResultSet res = stmt.executeQuery(consulta);
-            while (res.next()) {
-                contactoActual = new Contacto(res.getString(1), res.getString(2), res.getString(3));
-                al_contactos.add(contactoActual);
-            }
-        } catch (SQLException e) {
-            System.out.println("[!] No se ha podido leer el contacto. Detalles: ");
-            e.printStackTrace();
-        }
-            // devuelve en un arraylist las coincidencias
-            return al_contactos;
-    }
-    
-public static ArrayList<Contacto> buscarPorEmailEnBD(String cadena) {
-        // busca los contactos que tienen esa cadena en su email (email LIKE '%'+cadena+'%')
-        // devuelve en un arraylist las coincidencias
-        ArrayList<Contacto> al_contactos = new ArrayList<Contacto>();
-        Contacto contactoActual;
-        try {
-            Statement stmt = conn.createStatement();
-            String consulta = "SELECT fullname, telephone, email FROM CONTACTS WHERE email LIKE '%" + cadena + "%'";
-            ResultSet res = stmt.executeQuery(consulta);
-            while (res.next()) {
-                contactoActual = new Contacto(res.getString(1), res.getString(2), res.getString(3));
-                al_contactos.add(contactoActual);
-            }
-        } catch (SQLException e) {
-            System.out.println("[!] No se ha podido leer el contacto. Detalles: ");
-            e.printStackTrace();
-        }
-        return al_contactos;
-    }
-
-    public static ArrayList<Contacto> buscarPorTelefonoEnBD(String cadena) {
-        // busca los contactos que tienen esa cadena en su telefono (telefono LIKE '%'+cadena+'%')
-        // devuelve en un arraylist las coincidencias
-        ArrayList<Contacto> al_contactos = new ArrayList<Contacto>();
-        Contacto contactoActual;
-        try {
-            Statement stmt = conn.createStatement();
-            String consulta = "SELECT fullname, telephone, email FROM CONTACTS WHERE telephone LIKE '%" + cadena + "%'";
-            ResultSet res = stmt.executeQuery(consulta);
-            while (res.next()) {
-                contactoActual = new Contacto(res.getString(1), res.getString(2), res.getString(3));
-                al_contactos.add(contactoActual);
-            }
-        } catch (SQLException e) {
-            System.out.println("[!] No se ha podido leer el contacto. Detalles: ");
-            e.printStackTrace();
-        }
-        return al_contactos;
-    }
-
-    public static ArrayList<Contacto> buscarGlobalEnBD(String cadenadetexto) {
-        // busca los contactos que tienen esa cadena en alguno de sus campos
-        // Para ello, llama a los método anteriores y concatena los resultados distintos
-        // devuelve un único arraylist con todas las coincidencias (de contactos con diferente id)
-        ArrayList<Contacto> al_contactos = new ArrayList<Contacto>();
-        Contacto contactoActual;
-        try {
-            Statement stmt = conn.createStatement();
-            String consulta = "SELECT fullname, telephone, email FROM CONTACTS WHERE fullname OR email OR telephone LIKE '%" + cadenadetexto + "%'";
-            ResultSet res = stmt.executeQuery(consulta);
-            while (res.next()) {
-                contactoActual = new Contacto(res.getString(1), res.getString(2), res.getString(3));
-                al_contactos.add(contactoActual);
-            }
-        } catch (SQLException e) {
-            System.out.println("[!] No se ha podido leer el contacto. Detalles: ");
-            e.printStackTrace();
-        }
-        return al_contactos;
+        return contactos;
     }
 }
